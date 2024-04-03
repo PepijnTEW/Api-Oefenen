e5d875a86b8ff68d196ca4a9ea3e725a7b4dd825
pepijnTEW <37923@ma-web.nl>
2024-04-03 20:29:36 +0200

    age data working

--- Main/js/main.js
+++ Main/js/main.js
@@ -1,10 +1,15 @@
 const pokemonImg = document.getElementById("js--pokemon-img");
 const catchButton = document.getElementById("js--catch-button");
+const newPokemon = document.getElementById("js--new-pokemon")
 const pokemonText = document.getElementById("js--pokemon-text");
 const buttonImg = document.getElementById("buttonImg");
+const inputFlied = document.getElementById("js--input");
+const nameText = document.getElementById("js--name");
 let randomNumber = Math.floor(Math.random() * 250 + 1);
 let gamePlayed = false;
 
+
+//Loads pokeApi
 let pokemon = fetch("https://pokeapi.co/api/v2/pokemon/"+ randomNumber)
     .then(function(responce){
         return responce.json();
@@ -14,18 +19,13 @@ let pokemon = fetch("https://pokeapi.co/api/v2/pokemon/"+ randomNumber)
         pokemonText.innerText = "A wild "+realData.name+" appeared!";
     });
 
-let age = fetch(" https://api.agify.io?name=michael")
-    .then(function (responce) {
-        return responce.json();
-    })
-    .then(function (ageData) {
-    console.log(ageData);
-    });
+//Handels everything on button click(Pokemon)
 catchButton.onclick = function () {
 
     let id = null;
     let pos = 0;
 
+    //Animation for Pokeball(Going up)
     if (gamePlayed === false) {
             clearInterval(id);
     id = setInterval(frame, 1);
@@ -36,7 +36,8 @@ catchButton.onclick = function () {
         pos = pos - 5;
         buttonImg.style.top = pos + 'px';
       }
-    }
+    } 
+        //Chances of catching the pokemon
         let catchNumber = Math.floor(Math.random() * 2);
         if (catchNumber === 0) {
             pokemonText.innerText = "Fled!";
@@ -46,6 +47,7 @@ catchButton.onclick = function () {
         }
     }
     gamePlayed = true;
+    //Animation for Pokeball(going back down)
     setTimeout(() => {
         clearInterval(id);
         id = setInterval(frame);
@@ -57,23 +59,40 @@ catchButton.onclick = function () {
               buttonImg.style.top = pos + 'px';
           }
         }
+        pokemonImg.src = '../imgs/ffffff.png';
     }, 500);
-    setTimeout(() => {
+}
 
-        if (gamePlayed === true) {
-            randomNumber = Math.floor(Math.random() * 250 + 1);
-            let pokemon = fetch("https://pokeapi.co/api/v2/pokemon/"+ randomNumber)
-            .then(function(responce){
-                return responce.json();
+//New Pokemon button
+newPokemon.onclick = function(){
+    if (gamePlayed === true) {
+        randomNumber = Math.floor(Math.random() * 250 + 1);
+        let pokemon = fetch("https://pokeapi.co/api/v2/pokemon/"+ randomNumber)
+        .then(function(responce){
+            return responce.json();
+    })
+    //Makes new Pokemon
+    .then(function(realData){
+        pokemonImg.src = realData.sprites.front_default;
+        pokemonText.innerText = "A wild "+realData.name+" appeared!";
+    });
+    }
+    gamePlayed = false;
+}
+
+//Agify Code
+
+inputFlied.onkeyup = function(event){
+    if(event.keyCode === 13){
+        //Loads Agify and configures it
+        let name = inputFlied.value
+        let age = fetch(" https://api.agify.io?name="+name)
+        .then(function (responce) {
+            return responce.json();
         })
-        .then(function(realData){
-            pokemonImg.src = realData.sprites.front_default;
-            pokemonText.innerText = "A wild "+realData.name+" appeared!";
+        .then(function (echteData) {
+            inputFlied.style.display = 'none';
+            nameText.innerText = echteData.age;
         });
-        }
-        gamePlayed = false;
-
-    }, 1000);
-
-    
+    }
 }
